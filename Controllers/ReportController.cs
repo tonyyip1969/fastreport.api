274f6a9c9790aa2b68ec99469d20bc6edd8ae077
tony.yip <tonyyip1969@gmail.com>
2021-12-28 06:02:02 +0800

    add sample fastreport

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,18 @@
+﻿using FastReport.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastReport.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReportController : ControllerBase
+    {
+
+        [HttpGet(Name = "GetReport")]
+        public MemoryStream Get()
+        {
+            var report = new ReportService();
+            return report.Generate();
+        }
+    }
+}
---
+++ Services/ReportService.cs
@@ -0,0 +1,50 @@
+﻿using FastReport.Export.PdfSimple;
+
+namespace FastReport.Api.Services
+{
+    public interface IReport
+    {
+        MemoryStream Generate();
+    }
+
+    public class ReportService : IReport
+    {
+        public MemoryStream Generate()
+        {
+            var strm = new MemoryStream();
+            var report = new Report();
+            try
+            {
+                var templatePath = ".\\Reports\\Sample.frx";
+                report.Report.Load(templatePath);
+                
+                //report.RegisterData(DataSet);
+
+                if (report.Prepare())
+                {
+                    Export(report, strm);
+                }
+            }
+            finally
+            {
+                report.Dispose();
+            }
+
+            return strm;
+        }
+
+        private void Export(Report report, MemoryStream memoryStream)
+        {
+            var pdfExport = new PDFSimpleExport();
+            try
+            {
+                pdfExport.Export(report, memoryStream);
+                memoryStream.Position = 0;
+            }
+            finally
+            {
+                pdfExport.Dispose();
+            }
+        }
+    }
+}
